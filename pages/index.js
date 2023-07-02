bae80f0630d4f9be101c06bf2f1524284cd3a114
Marcio Bueno <marciobueno@gmail.com>
2023-07-02 02:43:13 +0000

    colocando uma frase na Home

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 const Home = () => {
-    return <h1>Teste</h1>;
+    return (
+        <>
+            <h1>Mozinho</h1>
+            <p>Esse aqui é o primeiro desafio do curso. Te amo, beijos.</p>
+        </>
+    );
 }
 
 export default Home;
