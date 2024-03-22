ad7c14544f5cffe267cc4e6d5e72e91cede63189
Marcio Bueno <marciobueno@gmail.com>
2024-03-22 00:11:33 +0000

    make `database.js` `client.connect()` more rob to errors

--- infra/database.js
+++ infra/database.js
@@ -1,20 +1,23 @@
 import { Client } from "pg";
 
 const query = async (queryObject) => {
-  const client = new Client({
+  const postgresCredentials = {
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-  });
-  await client.connect();
+  };
+  const client = new Client(postgresCredentials);
+  console.log("Credenciais do Postgres:", postgresCredentials);
 
   try {
+    await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
     console.error(error);
+    throw error;
   } finally {
     await client.end();
   }
